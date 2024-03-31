2a6d09156856b65403d58ac2072328247b85b8ec
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-03-31 23:24:52 +0200

    Add files via upload

---
+++ Console1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Type t = Type.GetTypeFromProgID("KSR20.COM3Klasa.1");
+        object k = Activator.CreateInstance(t);
+        object[] args = new object[] { "Dziala OK" };
+        t.InvokeMember("Test", System.Reflection.BindingFlags.InvokeMethod, null, k, args);
+    }
+}
